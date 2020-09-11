799b2f9b7379df712720944fed804b49c4fc8424
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-09-11 17:48:06 -0500

    Switching computers
    Pushing to cloud so I have access on a different computer.

--- welcome-page/progressBar.js
+++ welcome-page/progressBar.js
@@ -1,14 +1,36 @@
+function popUpProggressBar() {
+  var elem = document.getElementsById('loadingmessage');
+  elem.innerHTML = `Loading ${elem.id}`;
+}
+
 function startProgressBar() {
-    var elem = document.getElementById("progressBar");   
-    var width = 0;
-    var id = setInterval(frame, 80);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-        elem.innerHTML = width * 1  + '%';
-      }
+  var elem = document.getElementById('progressbar');
+  var width = 0;
+  var x = setInterval(function() {
+    if (width >= 80) {
+      clearInterval(x);
+      elem.innerHTML = 'ERROR: Verification Failed!';
+      setTimeout(() => { window.location.replace('../killed-terminal-page/killed-terminal.html'); }, 5000);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1 + '%';
     }
-  }
+  }, 80);
+}
+
+// width: 600px;
+// height: 300px;
+// padding: 40px;
+// position: absolute;
+// top: 50%;
+// left: 50%;
+// transform: translate(-50%, -50%);
+// text-align: center;
+// background: black;
+// border-radius: 24px;
+// border: 2px solid red;
+// font-family: Arial, Helvetica, sans-serif;
+// font-size: 40px;
+// color: white;
+// opacity: 0.9;
