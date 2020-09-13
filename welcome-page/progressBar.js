b32104ea94703440136573d23a102a4e70121fd4
Angoooose <68403129+Angoooose@users.noreply.github.com>
2020-09-13 16:50:28 -0500

    Updated welcome.html, welcomstyle.css, and progressBar.js
    On click of a desktop icon it will now pop up with a progress bar.

--- welcome-page/progressBar.js
+++ welcome-page/progressBar.js
@@ -1,9 +1,19 @@
-function popUpProggressBar() {
-  var elem = document.getElementsById('loadingmessage');
-  elem.innerHTML = `Loading ${elem.id}`;
+function popUpProgressBar() {
+  var elem = document.getElementById('progressbox');
+  elem.style.display = 'block';
 }
 
-function startProgressBar() {
+function updateLoadingMessage(name) {
+  var elem = document.getElementById('loadingmessage');
+  elem.innerHTML = `Loading ${name}...`;
+  if (name === 'Thunderbird') {
+    elem.style.fontSize = '50px';
+  }
+}
+
+function startProgressBar(name) {
+  popUpProgressBar();
+  updateLoadingMessage(name);
   var elem = document.getElementById('progressbar');
   var width = 0;
   var x = setInterval(function() {
@@ -18,19 +28,3 @@ function startProgressBar() {
     }
   }, 80);
 }
-
-// width: 600px;
-// height: 300px;
-// padding: 40px;
-// position: absolute;
-// top: 50%;
-// left: 50%;
-// transform: translate(-50%, -50%);
-// text-align: center;
-// background: black;
-// border-radius: 24px;
-// border: 2px solid red;
-// font-family: Arial, Helvetica, sans-serif;
-// font-size: 40px;
-// color: white;
-// opacity: 0.9;
