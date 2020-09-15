f8f5de49eb5c97095cf88d73ed49ea5804c2dead
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-09-15 18:52:14 -0500

    Updated killed-terminal.html and killTimer.js

--- killed-terminal-page/killTimer.js
+++ killed-terminal-page/killTimer.js
@@ -1,11 +1,12 @@
 function killTerminalCountdown() {
     var elem = document.getElementById('terminalkilledmsg');
-    setTimeout(() => { elem.innerHTML = 'C:\secure-terminal\>Terminal Killed....'; }, 3000).then(connectionFailed());
+    setTimeout(() => { elem.innerHTML = 'C:\\secure-terminal\\>Terminal Killed....'; }, 3000).then(connectionFailed());
 }
 function connectionFailed() {
-    setTimeout(() => { document.getElementById('connection1').innerHTML = 'ERROR: Connection failed, attempting to reconect.'; }, 5000);
-    setTimeout(() => { document.getElementById('connection2').innerHTML = 'ERROR: Connection failed, attempting to reconect.'; }, 10000);
-    setTimeout(() => { document.getElementById('connection3').innerHTML = 'ERROR: Connection failed, attempting to reconect.'; }, 15000);
-    setTimeout(() => { document.getElementById('connection4').innerHTML = 'ERROR: Connection failed, attempting to reconect.'; }, 20000);
-    setTimeout(() => { document.getElementById('connection5').innerHTML = 'ERROR: Connection failed, attempting to reconect.'; }, 25000);
+    setTimeout(() => { document.getElementById('connection1').innerHTML = 'ERROR: Connection failed, attempting to reconnect (Failed attempts: 1).'; }, 5000);
+    setTimeout(() => { document.getElementById('connection2').innerHTML = 'ERROR: Connection failed, attempting to reconnect (Failed attempts: 2).'; }, 10000);
+    setTimeout(() => { document.getElementById('connection3').innerHTML = 'ERROR: Connection failed, attempting to reconnect (Failed attempts: 3).'; }, 15000);
+    setTimeout(() => { document.getElementById('connection4').innerHTML = 'ERROR: Connection failed, attempting to reconnect (Failed attempts: 4).'; }, 20000);
+    setTimeout(() => { document.getElementById('connection5').innerHTML = 'ERROR: Connection failed, attempting to reconnect (Failed attempts: 5).'; }, 25000);
+    setTimeout(() => { document.getElementById('connection6').innerHTML = 'ERROR: Failed to reconnect after 5 attempts...'; }, 30000);
 }
