c25974c6e4f6be2f7cb5b4113eaccb3b19997183
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-09-15 18:41:45 -0500

    Finished login system
    You now require a specified Agent ID and Agent Password to get the Desktop.

--- login-page/loginCheck.js
+++ login-page/loginCheck.js
@@ -1,7 +1,17 @@
-function loginCheck(username, password) {
-    if (username === 'test') {
-        if (password === '1234') {
-
+function loginCheck() {
+    const userAgentId = 'agent1234';
+    const userAgentPassword = 'password1234';
+    var agentId = document.getElementById('agentid').value;
+    var password = document.getElementById('password').value;
+    if (agentId === userAgentId) {
+        if (password === userAgentPassword) {
+            window.location.replace('../welcome-page/welcome.html');
         }
+    } else if (agentId != userAgentId) {
+        document.getElementById('failedlogin').style.display = 'block';
     }
+}
+
+function hideFailedLogin() {
+    document.getElementById('failedlogin').style.display = 'none';
 }
